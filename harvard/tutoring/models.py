ebd85069e0808fd73d051413c2f3d5be02a152d3
Santiago Paiva <paiva.santiago@gmail.com>
2016-04-17 14:42:42 -0400

    add Course, School, Department models

--- harvard/tutoring/models.py
+++ harvard/tutoring/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    course_code = models.CharField(max_length=50)
+    course_name = models.CharField(max_length=200)
+    department = models.CharField(max_length=200)
+    school = models.CharField(max_length=200)
+
+class Department(models.Model):
+    department_name = models.CharField(max_length=200)
+    department_courses = models.IntegerField(default=0)
+
+class School(models.Model):
+    school_name = models.CharField(max_length=200)
+    school_courses = models.IntegerField(default=0)

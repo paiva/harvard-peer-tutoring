6a1b494d8300d67d4cf342bb042a6aa8b34ecdad
Santiago Paiva <paiva.santiago@gmail.com>
2016-04-17 14:50:17 -0400

    first migration

--- harvard/harvard/settings.py
+++ harvard/harvard/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tutoring',
+    'tutoring.apps.TutoringConfig',
 ]
 
 MIDDLEWARE_CLASSES = [

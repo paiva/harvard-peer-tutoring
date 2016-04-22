bc9219c853205749301fa7eda9114a1e083930dc
sap789 <sap789@g.harvard.edu>
2016-04-22 11:20:27 -0400

    models improvement

--- harvard/tutoring/models.py
+++ harvard/tutoring/models.py
@@ -1,15 +1,27 @@
 from django.db import models
 
 class Course(models.Model):
-    course_code = models.CharField(max_length=50)
-    course_name = models.CharField(max_length=200)
-    department = models.CharField(max_length=200)
-    school = models.CharField(max_length=200)
+    
+	course_code = models.CharField(max_length=50)
+	course_name = models.CharField(max_length=200)
+	department = models.CharField(max_length=200)
+	school = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.course_code
 
 class Department(models.Model):
-    department_name = models.CharField(max_length=200)
-    department_courses = models.IntegerField(default=0)
+    
+	department_name = models.CharField(max_length=200)
+	department_courses = models.IntegerField(default=0)
+
+	def __str__(self):
+		return self.department_name
+
 
 class School(models.Model):
-    school_name = models.CharField(max_length=200)
-    school_courses = models.IntegerField(default=0)
+	school_name = models.CharField(max_length=200)
+	school_courses = models.IntegerField(default=0)
+
+	def __str__(self):
+		return self.school_name

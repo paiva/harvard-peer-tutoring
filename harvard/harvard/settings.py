cbdce3d8dd604c8b6f96bc4009486161ca538fec
Santiago Paiva <paiva.santiago@gmail.com>
2016-04-22 23:00:26 -0400

    add templates

--- harvard/harvard/settings.py
+++ harvard/harvard/settings.py
@@ -124,4 +124,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+STATIC_ROOT = ''
 STATIC_URL = '/static/'
+STATICFILES_DIRS = ( os.path.join('static'),)
+
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]

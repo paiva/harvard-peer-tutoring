009b3f2f0ad164524053e42e787416a5e4332480
sap789 <sap789@g.harvard.edu>
2016-05-08 17:22:35 -0400

    code cleanup

--- harvard/search/views.py
+++ harvard/search/views.py
@@ -1,8 +1,11 @@
 from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.views.generic import View
 from .models import Course, Department, School
-
+from .forms import UserForm
 
 class IndexView(generic.ListView):
 	template_name = 'search/home.html'
@@ -29,3 +32,39 @@ class DepartmentUpdate(UpdateView):
 class DepartmentDelete(DeleteView):
 	model = Department
 	success_url = reverse_lazy('search:index')
+
+class UserFormView(View):
+	form_class = UserForm
+	template_name = 'search/signup.html'
+
+	# Display blank form to the user
+	def get(self,request):
+		form = self.form_class(None)
+		return render(request, self.template_name, {'form':form})
+
+	# Process form data
+	def post(self,request):
+		form = self.form_class(request.POST)
+
+		if form.is_valid():
+
+			user = form.save(commit=False)
+
+			# Clean normalized data
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password']
+			user.set_password(password)
+			user.save()
+
+			# Returns User objects if credentials are correct
+			user = autheticate(username=username, password=password)
+
+			if user is not None:
+
+				if user.is_active:
+					login(request,user)
+					return redirect('search:index')
+		return render(request, self.template_name, {'form':form})
+
+
+

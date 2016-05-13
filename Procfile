3ab1703210e67d90275e9ecb46c9a4aed2e02442
Santiago Paiva <paiva.santiago@gmail.com>
2016-05-13 10:50:46 -0400

    add procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn harvard.wsgi:application --log-file -
--- harvard/settings.py
+++ harvard/settings.py
@@ -84,6 +84,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
